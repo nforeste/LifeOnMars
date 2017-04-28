c8f928c508598f7e2a920d69d4b8b5ccea0b1146
Nick Forester <nforeste@ucsc.edu>
2017-04-28 16:57:48 -0700

    Implemented highlight on mouseover for the grid

--- js/prefabs/grid.js
+++ js/prefabs/grid.js
@@ -1,3 +1,5 @@
+var v1, v2;
+
 function Grid(w, h, lineW, lineC) {
 	this.w = w;
 	this.h = h;
@@ -5,13 +7,21 @@ function Grid(w, h, lineW, lineC) {
 	this.bmd = game.add.bitmapData(game.world.width, game.world.height);
 	this.bmd.ctx.lineWidth = lineW;
 	this.bmd.ctx.strokeStyle = lineC;
-	game.add.sprite(0, 0, this.bmd);
+	this.sprite = game.add.sprite(0, 0, this.bmd);
 
-	//this.tile = game.add.tileSprite(0, 0, game.world.width, game.world.height, this.bmd);
+	//create a 2-Dimensional matrix to represent the grid
+	//this will be used for terrain (cells[3][4] is a mountain)
+	this.cells = [];
+	for (let i = 0; i < game.world.width / w; i++) {
+		this.cells[i] = [];
+	}
+
+	this.index1 = -1;
+	this.index2 = -1;
 }
 
 //Draws the grid on screen 
-Grid.prototype.draw = function() {
+Grid.prototype.makeGrid = function() {
 	for (let i = 0; i < game.world.width; i += this.w) {
 		this.bmd.ctx.moveTo(i, 0);
 		this.bmd.ctx.lineTo(i, game.world.height);
@@ -28,4 +38,23 @@ Grid.prototype.draw = function() {
 Grid.prototype.update = function() {
 	//Either use tileSprite, OR bigger bitmapData with camera scrolling
 	//camera scrolling is probably better option in my opinion
+	this.upperLeftRow = Math.ceil(game.camera.view.x / this.w);
+	this.upperLeftColumn = Math.ceil(game.camera.view.y / this.h);
+
+	//vars for debugging
+	v1 = this.upperLeftRow;
+	v2 = this.upperLeftColumn;
+
+	if (this.index1 >= 0 && this.index2 >= 0) {
+		this.bmd.ctx.clearRect((this.w * this.index1) + 1, (this.h * this.index2) + 1, this.w - 2, this.h - 2);
+	}
+
+	//For now. This will be tweaked to include when picking up an object, etc.
+	//Only draw rects when mouse is on the screen
+	if (game.input.activePointer.withinGame) {
+		this.index1 = Math.floor(game.input.x / this.w) + this.upperLeftRow;
+		this.index2 = Math.floor(game.input.y / this.h) + this.upperLeftColumn;
+		this.bmd.rect((this.w * this.index1) + 1, (this.h * this.index2) + 1, 
+			this.w - 2, this.h - 2, '#0000ff');
+	}
 };
--- js/states/play.js
+++ js/states/play.js
@@ -9,12 +9,34 @@ Play.prototype = {
 	},
 	create: function() {
 		console.log('Play: create()');
+		game.world.setBounds(0, 0, 2000, 2000);
+
 		//orangish brown
 		this.stage.backgroundColor = '#c1440e';
 		this.g = new Grid(40, 40, 1, 'black');
-		this.g.draw();
+		this.g.makeGrid();
+
+		this.cursors = this.input.keyboard.createCursorKeys();
 	},
 	update: function() {
 		this.g.update();
+
+		if (this.cursors.down.isDown) {
+			this.camera.y += 5;
+		}
+		if (this.cursors.up.isDown) {
+			this.camera.y -= 5;
+		}
+		if (this.cursors.right.isDown) {
+			this.camera.x += 5;
+		}
+		if (this.cursors.left.isDown) {
+			this.camera.x -= 5;
+		}
+	},
+	render: function() {
+		game.debug.text(game.input.activePointer.position, 2, 14, '#ffffff');
+		game.debug.cameraInfo(this.camera, 2, 64, '#ffffff');
+		game.debug.text('row: ' + v1 + ', col: ' + v2, 2, 180, '#ffffff');
 	}
 };
