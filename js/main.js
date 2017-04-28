475de277353ce0173ded3b9a0e47838321ef3ad2
Nick Forester <nforeste@ucsc.edu>
2017-04-28 10:39:12 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var game;
+
+
+
+
+window.onload = function() {
+	//Game resolution is arbitrary at the moment
+	game = new Phaser.Game(800, 600, Phaser.CANVAS);
+	game.state.add('Play', Play);
+	game.state.start('Play');
+}
---
+++ js/states/play.js
@@ -0,0 +1,16 @@
+/*
+Play is the state containing the main game loop
+*/
+
+function Play() {};
+Play.prototype = {
+	preload: function() {
+		console.log('Play: preload()');
+	},
+	create: function() {
+		console.log('Play: create()');
+	},
+	update: function() {
+
+	}
+};
