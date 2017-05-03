6d5a9e5d56acd54d76ed0a8e47c076d4410b42c0
Nick Forester <nforeste@ucsc.edu>
2017-05-02 19:44:45 -0700

    fixed some bugs with the grid

--- js/prefabs/grid.js
+++ js/prefabs/grid.js
@@ -1,78 +1,77 @@
-var xTiles, yTiles;
-
 function Grid(w, h, lineC) {
-	this.w = w;
-	this.h = h;
+    this.w = w;
+    this.h = h;
 
-	this.bmd = game.add.bitmapData(game.world.width, game.world.height);
-	this.bmd.ctx.strokeStyle = lineC;
+    //bmdGrid is the bitmapData that draws the grid on the screen
+    this.bmdGrid = game.add.bitmapData(game.world.width, game.world.height);
+    this.bmdGrid.ctx.strokeStyle = lineC;
 
-	//lineWidth was going to be an argument, but it broke everything :/
-	this.bmd.ctx.lineWidth = 1;
-	this.sprite = game.add.sprite(0, 0, this.bmd);
+    //lineWidth was going to be an argument, but it broke everything :/
+    this.bmdGrid.ctx.lineWidth = 1;
 
-	//create a 2-Dimensional matrix to represent the grid
-	//this will be used for terrain (cells[3][4] is a mountain)
-	this.cells = [];
-	for (let i = 0; i < game.world.width / w; i++) {
-		this.cells[i] = [];
-	}
+    //create a 2-Dimensional matrix to represent the grid
+    //this will be used for terrain (cells[3][4] is a mountain)
+    this.cells = [];
+    for (let i = 0; i < game.world.width / w; i++) {
+        this.cells[i] = [];
+    }
 
-	this.index1 = -1;
-	this.index2 = -1;
+    //bmdOverlay is the bitmapData that draws the highlights on the grid
+    this.bmdOverlay = game.add.bitmapData(game.world.width, game.world.height);
+    this.sprite = game.add.sprite(0, 0, this.bmdOverlay);
+    game.add.sprite(0, 0, this.bmdGrid);
 
-	//TESTING
-	xTiles = 3;
-	yTiles = 1;
+    this.index1 = -1;
+    this.index2 = -1;
 }
 
 //Draws the grid on screen 
 Grid.prototype.makeGrid = function() {
-	for (let i = 0; i < game.world.width; i += this.w) {
-		this.bmd.ctx.moveTo(i, 0);
-		this.bmd.ctx.lineTo(i, game.world.height);
-		this.bmd.ctx.stroke();
-	}
+    for (let i = 0; i < game.world.width; i += this.w) {
+        this.bmdGrid.ctx.moveTo(i, 0);
+        this.bmdGrid.ctx.lineTo(i, game.world.height);
+        this.bmdGrid.ctx.stroke();
+    }
 
-	for (let i = 0; i < game.world.height; i += this.h) {
-		this.bmd.ctx.moveTo(0, i);
-		this.bmd.ctx.lineTo(game.world.width, i);
-		this.bmd.ctx.stroke();
-	}
+    for (let i = 0; i < game.world.height; i += this.h) {
+        this.bmdGrid.ctx.moveTo(0, i);
+        this.bmdGrid.ctx.lineTo(game.world.width, i);
+        this.bmdGrid.ctx.stroke();
+    }
 };
 
-Grid.prototype.draw = function() {
-	this.upperLeftRow = Math.ceil(game.camera.view.x / this.w);
-	this.upperLeftColumn = Math.ceil(game.camera.view.y / this.h);
-	this.offsetx = game.camera.view.x % this.w;
-	this.offsety = game.camera.view.y % this.h;
+Grid.prototype.draw = function(xTiles, yTiles) {
+    this.upperLeftRow = Math.ceil(game.camera.view.x / this.w);
+    this.upperLeftColumn = Math.ceil(game.camera.view.y / this.h);
+    this.offsetx = game.camera.view.x % this.w;
+    this.offsety = game.camera.view.y % this.h;
 
-	if (this.index1 >= 0 && this.index2 >= 0) {
-		for (let i = this.xStart; i < this.xStart + xTiles; i++) {
-			for (let j = this.yStart; j < this.yStart + yTiles; j++) {
-				this.bmd.ctx.clearRect((this.w * i) + 1, (this.h * j) + 1, this.w - 2, this.h - 2);
-			}
-		}
-	}
+    if (this.index1 >= 0 && this.index2 >= 0) {
+        for (let i = this.xStart; i < this.xStart + xTiles; i++) {
+            for (let j = this.yStart; j < this.yStart + yTiles; j++) {
+                this.bmdOverlay.ctx.clearRect(this.w * i, this.h * j, this.w, this.h);
+            }
+        }
+    }
 
-	//For now. This will be tweaked to include when picking up an object, etc.
-	//Only draw rects when mouse is on the screen
-	if (game.input.activePointer.withinGame) {
-		//If the camera is scrolled so that it doesn't line up with the grid
-		//i.e. it ends in the middle of one row (cutting it off)
-		//then subtract the offset so that the mouse position still correctly finds the index
-		this.index1 = Math.floor((game.input.x - (this.offsetx !== 0 ? this.w - this.offsetx : 0)) / this.w) + this.upperLeftRow;
-		this.index2 = Math.floor((game.input.y - (this.offsety !== 0 ? this.h - this.offsety : 0)) / this.h) + this.upperLeftColumn;
+    //For now. This will be tweaked to include when picking up an object, etc.
+    //Only draw rects when mouse is on the screen
+    if (game.input.activePointer.withinGame) {
+        //If the camera is scrolled so that it doesn't line up with the grid
+        //i.e. it ends in the middle of one row (cutting it off)
+        //then subtract the offset so that the mouse position still correctly finds the index
+        this.index1 = Math.floor((game.input.x - (this.offsetx !== 0 ? this.w - this.offsetx : 0)) / this.w) + this.upperLeftRow;
+        this.index2 = Math.floor((game.input.y - (this.offsety !== 0 ? this.h - this.offsety : 0)) / this.h) + this.upperLeftColumn;
 
-		//update the grid offsets with the new index positions
-		this.xStart = Math.max(this.index1 - (xTiles > 2 ? Math.floor(xTiles / 2) : 0), 0);
-		this.yStart = Math.max(this.index2 - (yTiles > 2 ? Math.floor(yTiles / 2) : 0), 0);
+        //update the grid offsets with the new index positions
+        this.xStart = Math.min(Math.max(this.index1 - Math.floor(xTiles / 2), 0), (game.world.width / this.w) - xTiles);
+        this.yStart = Math.min(Math.max(this.index2 - Math.floor(yTiles / 2), 0), (game.world.height / this.h) - yTiles);
 
-		//draw all of the rectangles
-		for (let i = this.xStart; i < this.xStart + xTiles; i++) {
-			for (let j = this.yStart; j < this.yStart + yTiles; j++) {
-				this.bmd.rect((this.w * i) + 1, (this.h * j) + 1, this.w - 2, this.h - 2, '#66ff33');
-			}
-		}
-	}
+        //draw all of the rectangles
+        for (let i = this.xStart; i < this.xStart + xTiles; i++) {
+            for (let j = this.yStart; j < this.yStart + yTiles; j++) {
+                this.bmdOverlay.rect(this.w * i, this.h * j, this.w, this.h, '#66ff33');
+            }
+        }
+    }
 };
--- js/states/play.js
+++ js/states/play.js
@@ -4,40 +4,39 @@ Play is the state containing the main game loop
 
 function Play() {};
 Play.prototype = {
-	preload: function() {
-		console.log('Play: preload()');
-	},
-	create: function() {
-		console.log('Play: create()');
-		game.world.setBounds(0, 0, 2000, 2000);
+    preload: function() {
+        console.log('Play: preload()');
+    },
+    create: function() {
+        console.log('Play: create()');
+        game.world.setBounds(0, 0, 2000, 2000);
 
-		//orangish brown
-		this.stage.backgroundColor = '#c1440e';
-		this.g = new Grid(40, 40, 'black');
-		this.g.makeGrid();
-	},
-	update: function() {
-		//this.g.draw();
+        //orangish brown
+        this.stage.backgroundColor = '#c1440e';
+        this.g = new Grid(40, 40, 'black');
+        this.g.makeGrid();
+    },
+    update: function() {
+        this.g.draw(4, 3);
 
 
-		if (this.input.activePointer.withinGame) {
-			if (this.input.x > this.camera.view.width - 50) {
-				this.camera.x += 5;
-			}
-			if (this.input.x < 50) {
-				this.camera.x -= 5;
-			}
-			if (this.input.y > this.camera.view.height - 50) {
-				this.camera.y += 5;
-			}
-			if (this.input.y < 50) {
-				this.camera.y -= 5;
-			}
-		}
-	},
-	render: function() {
-		//game.debug.text(game.input.activePointer.position, 2, 14, '#ffffff');
-		//game.debug.cameraInfo(this.camera, 2, 64, '#ffffff');
-		game.debug.text('x tiles: '+xTiles+', y tiles: '+yTiles, 2, 14, '#ffffff');
-	}
+        if (this.input.activePointer.withinGame) {
+            if (this.input.x > this.camera.view.width - 50) {
+                this.camera.x += 5;
+            }
+            if (this.input.x < 50) {
+                this.camera.x -= 5;
+            }
+            if (this.input.y > this.camera.view.height - 50) {
+                this.camera.y += 5;
+            }
+            if (this.input.y < 50) {
+                this.camera.y -= 5;
+            }
+        }
+    },
+    render: function() {
+        //game.debug.text(game.input.activePointer.position, 2, 14, '#ffffff');
+        //game.debug.cameraInfo(this.camera, 2, 64, '#ffffff');
+    }
 };
