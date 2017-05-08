845def14699684bf099fa05589b9ec3da4a45c64
Nick Forester <nforeste@ucsc.edu>
2017-05-08 11:42:58 -0700

    simplified some stuff

--- js/states/play.js
+++ js/states/play.js
@@ -10,8 +10,6 @@ var Play = function(game) {
     //the scale of the world (changes with zooming)
     this.worldScale = 1;
     this.zoomLevel = 0;
-    this.mapSizeX = 4032;
-    this.mapSizeY = 4032;
     this.dragSpeed = 4;
 };
 Play.prototype = {
@@ -64,8 +62,8 @@ Play.prototype = {
         //move the camera as the mouse goes to the sides of the screen
         //also scroll the background grid at the same frequency
         if (this.input.activePointer.withinGame) {
-            var oldCameraPosX = this.camera.position.x;
-            var oldCameraPosY = this.camera.position.y;
+            var oldCameraPosX = this.camera.x;
+            var oldCameraPosY = this.camera.y;
             if (this.input.x > this.camera.view.width - 50) {
                 this.camera.x += this.dragSpeed;
                 this.g.gridsSpr[this.zoomLevel].tilePosition.x -= Math.abs(this.camera.x - oldCameraPosX);
@@ -83,33 +81,36 @@ Play.prototype = {
                 this.g.gridsSpr[this.zoomLevel].tilePosition.y += Math.abs(this.camera.y - oldCameraPosY);
             }
         }
-        //console.log(this.g.gridsSpr[this.zoomLevel].tilePosition);
 
         //keep the grid tileSprite centered on the camera
         this.g.gridsSpr[this.zoomLevel].x = this.camera.view.x;
         this.g.gridsSpr[this.zoomLevel].y = this.camera.view.y;
     },
     render: function() {
-        game.debug.text(game.input.activePointer.position, 200, 14, '#ffffff');
-        game.debug.cameraInfo(this.camera, 2, 14, '#ffffff');
+        //game.debug.text(game.input.activePointer.position, 200, 14, '#ffffff');
+        //game.debug.cameraInfo(this.camera, 2, 14, '#ffffff');
+        //game.time.advancedTiming=true;
+        //game.debug.text(game.time.fps || '--',2,14,'#ffffff');
     },
     zoomIn: function() {
         if (this.worldScale < 2) {
             //store the current camera position
-            var oldCameraPosX = this.camera.position.x;
-            var oldCameraPosY = this.camera.position.y;
+            var oldCameraPosX = this.camera.x;
+            var oldCameraPosY = this.camera.y;
 
             //increase the world scale by a factor of 50%
             this.worldScale += .5;
+
+            this.g.bmdOverlay.clear();
+            this.g.gridsSpr[this.zoomLevel].kill();
 
             //arbitrary
             //try to find a closer (better) solution
             this.camera.x += Math.round(this.input.x / this.worldScale);
             this.camera.y += Math.round(this.input.y / this.worldScale);
 
-
             //Move the grid by the amount the camera moved in the opposite direction
-            if (oldCameraPosX !== this.camera.position.x || oldCameraPosY !== this.camera.position.y) {
+            if (oldCameraPosX !== this.camera.x || oldCameraPosY !== this.camera.y) {
                 this.g.gridsSpr[this.zoomLevel].tilePosition.x -= Math.abs(this.camera.x - oldCameraPosX);
                 this.g.gridsSpr[this.zoomLevel].tilePosition.y -= Math.abs(this.camera.y - oldCameraPosY);
             }
@@ -117,8 +118,6 @@ Play.prototype = {
             //for some reason this needs to be like this instead
             //of exactly like in zoomOut
             //if anyone can find a solution for that pls do
-            this.g.bmdOverlay.clear();
-            this.g.gridsSpr[this.zoomLevel].kill();
             this.zoomLevel++;
             this.g.gridsSpr[this.zoomLevel].tilePosition = this.g.gridsSpr[this.zoomLevel - 1].tilePosition;
 
@@ -132,8 +131,8 @@ Play.prototype = {
     zoomOut: function() {
         if (this.worldScale > 1) {
             //store the current camera position
-            var oldCameraPosX = this.camera.position.x;
-            var oldCameraPosY = this.camera.position.y;
+            var oldCameraPosX = this.camera.x;
+            var oldCameraPosY = this.camera.y;
 
             //decrease the world scale by a factor of 50%
             this.worldScale -= .5;
@@ -149,11 +148,11 @@ Play.prototype = {
             this.camera.y -= Math.round(this.camera.height / 4 / this.worldScale);
 
             //move the grid by the amount the camera moved in the opposite direction
-            if (oldCameraPosX !== this.camera.position.x || oldCameraPosY !== this.camera.position.y) {
+            if (oldCameraPosX !== this.camera.x || oldCameraPosY !== this.camera.y) {
                 this.g.gridsSpr[this.zoomLevel].tilePosition.x += Math.abs(this.camera.x - oldCameraPosX);
                 this.g.gridsSpr[this.zoomLevel].tilePosition.y += Math.abs(this.camera.y - oldCameraPosY);
             }
-            
+
             //Make the new grid visible
             this.g.gridsSpr[this.zoomLevel].revive();
 
