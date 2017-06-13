bce4073e9b6e9ac3b152f1727823355304ceeca7
Nick Forester <nforeste@ucsc.edu>
2017-06-12 19:16:48 -0700

    styled the main game page

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@
 var game;
 
 window.onload = function() {
-    //Game resolution is arbitrary at the moment
-    game = new Phaser.Game(800, 600, Phaser.CANVAS);
-    game.state.add('Menu', Menu);
-    game.state.add('Play', Play);
-    game.state.add('GameOver', GameOver);
-    game.state.start('Menu');
+	//Game resolution is arbitrary at the moment
+	game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game');
+	game.state.add('Menu', Menu);
+	game.state.add('Play', Play);
+	game.state.add('GameOver', GameOver);
+	game.state.start('Menu');
 };
--- js/prefabs/building.js
+++ js/prefabs/building.js
@@ -818,7 +818,7 @@ WaterTank2x1.prototype.updateFrame = function() {
     var waterMax = this._game.resources.water.storage;
     this.ratio = waterCurrent / waterMax;
     this.ratio = Math.floor(this.ratio * 9);
-    this.ratio = Math.clamp(this.ratio, 0, 8);
+    this.ratio = Phaser.Math.clamp(this.ratio, 0, 8);
     this.frameName = (this.rotated > 0 ? 'WaterTankLeftRight' : 'WaterTankUpDown') + this.ratio;
 };
 
@@ -1034,7 +1034,7 @@ Storage2x2.prototype.updateFrame = function() {
     var storageMax = this._game.resources.food.storage + this._game.resources.mat.storage;
     this.ratio = storageCurrent / storageMax;
     this.ratio = Math.floor(this.ratio * 17);
-    this.ratio = Math.clamp(this.ratio, 0, 16);
+    this.ratio = Phaser.Math.clamp(this.ratio, 0, 16);
     this.frameName = 'Storage' + this.ratio;
 };
 
